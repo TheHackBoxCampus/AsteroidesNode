9159f0ebd9fe6a6faadffef4d1784d21906e18b6
TheHackBoxCampus <millercampusdeveloper@gmail.com>
2023-07-03 21:19:24 -0500

    feat: :sparkles: Filtrado por valor, no por coincidencia

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import http from 'http'; 
 import https from 'https'; 
-import path from 'path';
+import {JSDOM} from 'jsdom'; 
 import fs from 'fs'; 
 
 let apik = "AWOPjMeOhAVw3FlgSvSYrgfmgtNZOGvuhN0dt1Jx"; 
@@ -11,44 +11,58 @@ let api_config = {
     hostname: `https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${apik}`
 }
 
-let server = http.createServer((req, res) => {
+const createDom = (structuredom) => {
+    // vars
+    let welcome = structuredom.createElement('h1');
+    let input = structuredom.createElement('input');
+    let container = structuredom.createElement('div');
+    let script = structuredom.createElement('script');
+    // attrs and text    
+    welcome.textContent += "Nasa"; 
+    input.setAttribute('placeholder', 'Busca el asteroide!!'); 
+    container.setAttribute('class', 'nasa-info'); 
+    container.setAttribute('style', "display: grid; grid-template-columns: repeat(3, 1fr); grid-gap: 3em");
+    container.textContent = "Cargando Info!"; 
+    script.setAttribute('src', './main.js'); 
+    return [welcome, input, container, script];
+}
 
+let server = http.createServer((req, res) => {
     // static files 
     let filePath = '.' + req.url; 
     if(filePath == "./home" || filePath == "./Home" || filePath == './') {
-        filePath = "./index.html";
+        // contentType format and dom virtual
         contentType = 'text/html'; 
+        let dom = new JSDOM('<!DOCTYPE html><html lang="es"><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><head></head><body></body></html>'); 
+       
+        // stament 
+        let document = dom.window.document;
 
-        fs.readFile(filePath, (err, content) => {
-            if(err) {
-                res.end('error: '+err);
-            }else{ 
-                res.writeHead(200, {"Content-Type": contentType})
-                res.end(content, 'utf-8');
-            }
-        })
-    } else if(filePath == './Api'){
-         // http request -> GET
+        // elements body 
+        let elements = createDom(document); 
+        document.body.append(...elements);
+  
+        // server use static files
+        let serialDOM = dom.serialize(); 
+        res.setHeader('Content-Type', contentType); 
+        res.statusCode = 200; 
+        res.end(serialDOM); 
+
+    } else if(filePath == './asteroides'){
+         // * http request -> GET
         let pet = https.get(api_config.hostname, (resq) => {
             data = ''; 
-            resq.on('data', (chunk) => {
-                data += chunk; 
-            })
-
-            resq.on('end', () => {
-                res.end(data); 
-            })
+            resq.on('data', (chunk) => data += chunk); 
+            resq.on('end', () => res.end(data)); 
         })
+        pet.on('error', (err) => console.log(err)); 
 
-        pet.on('error', (err) => {
-            console.log(err); 
-        })
+    // * structure main js - static file - javascript 
     } else if(filePath == './main.js') {
         contentType = "text/javascript"; 
         fs.readFile(filePath, (err, content) => {
-            if(err) {
-                res.end('error: '+err);
-            }else{ 
+            if(err) res.end('error: '+err);
+            else{ 
                 res.writeHead(200, {"Content-Type": contentType})
                 res.end(content, 'utf-8');
             }
@@ -56,13 +70,13 @@ let server = http.createServer((req, res) => {
     }
 }); 
 
-
 // server url 
 let config = {
     hostname: "127.223.22",
     port: 5013
 }
 
+// listener in server
 server.listen(config, () => {
     console.log('server listening in rute URL => http://'+config.hostname+":"+config.port); 
 })
--- main.js
+++ main.js
@@ -1,36 +1,71 @@
-let data = '';
-let btn = document.querySelector('.btn');
+let btn = document.querySelector('button'); 
+let container = document.querySelector('.nasa-info'); 
 
 const getData = async () =>{
-    let pet = await (await fetch("http://127.223.0.22:5013/Api", {
+    let pet = await (await fetch("http://127.223.0.22:5013/asteroides", {
         method : "GET",
         headers: {
             "Content-Type" : "application/json"
         }
     })).json(); 
-    return pet
+    return pet; 
 }
 
-const template = id => name => name_limited => designation => {
+const template = id => name => name_limited => designation => diameter => danger =>{
     return /* html */ `
-        <span>id: ${id}</span><br>
-        <span>nombre: ${name}</span><br>
-        <span>name limitado: ${name_limited}</span><br>
-    `;
+        <table style="margin: 3em 0; border: 1px solid #000;">
+            <tr style="color: red; text-align: center">
+                <th style="border: 1px solid black">id</th>
+                <th style="border: 1px solid black">name</th>
+                <th style="border: 1px solid black">name Limited</th>
+                <th style="border: 1px solid black">Designation</th>
+                <th style="border: 1px solid black">Diameter</th>
+                <th style="border: 1px solid black">Danger</th>
+            </tr>
+            <tr style="text-align: center">
+                <td style="border: 1px solid black">${id}</td>
+                <td style="border: 1px solid black">${name}</td>
+                <td style="border: 1px solid black">${name_limited}</td>
+                <td style="border: 1px solid black">${designation}</td>
+                <td style="border: 1px solid black">${diameter}</td>
+                <td style="border: 1px solid black">${danger}</td>
+            </tr>
+        </table>
+        `;
 }
-
-const renderData = () => {
-    let result = false; 
-    let container = document.querySelector('.nasa-info'); 
-    Promise.resolve(getData()).then(data => {
-        if('near_earth_objects' in data) {
-            result = !result; 
-            for(let properties in data['near_earth_objects']) {
-                let htmlRender = template(...properties);
-                container.innerHTML = htmlRender; 
-            }
+        const filter = () => {
+            let inpt = document.querySelector('input'); 
+            inpt.addEventListener('change', async (e) => {
+                container.innerHTML = "Buscando Data"; 
+                let data = await (getData()); 
+                let html = ''; 
+                for(properties of data.near_earth_objects) {
+                    if(properties.name_limited == inpt.value) {
+                        html += template(properties.id)(properties.name)(properties.name_limited)(properties.designation)(properties.estimated_diameter.kilometers.estimated_diameter_min)(properties.is_potentially_hazardous_asteroid);
+                    }else if(inpt.value == "") {
+                        container.innerHTML = "la data se restaura!!"; 
+                        html += template(properties.id)(properties.name)(properties.name_limited)(properties.designation)(properties.estimated_diameter.kilometers.estimated_diameter_min)(properties.is_potentially_hazardous_asteroid);
+                    }
+                }
+                container.innerHTML = ""; 
+                container.innerHTML += html; 
+            })
         }
+        const renderData = () => {
+                let result = false; 
+                Promise.resolve(getData()).then(data => {
+                    if('near_earth_objects' in data) {
+                        result = !result; 
+                        let htmlRender = ''; 
+            for(let properties of data['near_earth_objects']) {
+                htmlRender += template(properties.id)(properties.name)(properties.name_limited)(properties.designation)(properties.estimated_diameter.kilometers.estimated_diameter_min)(properties.is_potentially_hazardous_asteroid);
+            }
+            container.innerHTML = htmlRender; 
+
+        } else container.innerHTML = "Error de API - Revise el codigo o la consulta..."; 
     }); 
 }
 
-btn.addEventListener('click', renderData); 
+// TODO: Execute
+renderData();
+filter(); 
